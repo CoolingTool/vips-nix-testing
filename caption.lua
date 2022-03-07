b14c93319f181dc55154ffbf7150cf09f09afa20
CoolingTool <CoolingTool@users.noreply.github.com>
2022-03-06 22:44:06 -0600

    NEW NEW NEW

--- caption.lua
+++ caption.lua
@@ -4,44 +4,57 @@ local uv = require 'uv'
 local Image = v.Image
 local alpha = require 'alpha'
 
-local function caption(i, o, text, opt)
+
+local function caption(i, o, str, opt)
 	local x = uv.hrtime()
 	
-	local img = Image.new_from_file(path.resolve(i))
-	
+	local isgif = path.extname(i) == '.gif'
 
-	local width = img:width()
+	local image = Image.new_from_file(path.resolve(i), isgif and {n = -1} or {})
+
+	local width = image:width()
 	local size = width / 10
 	local text_dpi = 600 * (width / 1000)
 	local text_width = width - ((width / 25) * 2)
 
-	local font_string = 'futura'
-	local font_path = 'assets/caption.otf'
+	p(page_height, delay)
 
-	local caption_text = text
+	local font_string, font_path = 'futura', 'assets/caption.otf'
 
-	local text = Image.text(caption_text, {
-		rgba = true,
-		align = 'centre',
-		font = font_string,
-		fontfile = font_path,
-		dpi = text_dpi,
-		width = text_width,})
+	-- actual image generation time
+
+	local text = Image.text(str, {
+		rgba = true, align = 'centre',
+		font = font_string, fontfile = font_path,
+		dpi = text_dpi, width = text_width
+	})
 
 	local caption_image = text:flatten{background = 255}
-		--:equal{0,0,0,0}:bandand():ifthenelse(255, text)
 		:gravity('centre', width, text:height() + size, {
-			extend = 'white',})
+			extend = 'white'
+		})
 
-	
-	if alpha.has(img) then
-		caption_image = alpha.add(caption_image)
+	if alpha.has(image) then caption_image = alpha.add(caption_image) end
+
+	local final
+	if isgif then
+		local frames = {}
+
+		local page_height = image:get('page-height')
+		local pages = image:height() / page_height
+
+		p(pages, page_height)
+
+		for i = 0, pages-1 do
+			local page = image:crop(0, i * page_height, width, page_height)
+			table.insert(frames, caption_image:join(page, 'vertical'))
+		end
+
+		final = Image.arrayjoin(frames, {across = 1})
+		final:set('page-height', page_height + caption_image:height())
+	else
+		final = caption_image:join(image, 'vertical')
 	end
-
-	local final = caption_image
-		:join(img, 'vertical')
-
-	
 	final:write_to_file(path.resolve(o), opt)
 
 	print('caption:', (uv.hrtime()-x)*1e-9)
--- dialog.lua
+++ dialog.lua
@@ -1,7 +1,10 @@
 local path = require 'path'
 local v = require 'vips'
 local uv = require 'uv'
+local alpha = require 'alpha'
 local Image = v.Image
+
+local darkbg = {54, 57, 63}
 
 local function dialog(o, text)
 	local x = uv.hrtime()
@@ -19,9 +22,9 @@ local function dialog(o, text)
 			local vis = text:sub(1, po)
 			local invis = text:sub(utf8.offset(text,po,2))
 	
-			dialog_text = '<span foreground="black">' .. vis .. '<span alpha="1">' .. invis .. '</span></span>'
+			dialog_text = '<span foreground="white">' .. vis .. '<span alpha="1">' .. invis .. '</span></span>'
 		else
-			dialog_text = '<span foreground="black">' .. text .. '</span>'
+			dialog_text = '<span foreground="white">' .. text .. '</span>'
 		end
 
 		local text = Image.text(dialog_text, {
@@ -35,7 +38,12 @@ local function dialog(o, text)
 		table.insert(frames, text)
 	end
 
-	final = Image.arrayjoin(frames, {across = 1}):flatten{background = {54, 57, 63}}
+	-- join together frames with fake antialias
+	local joined = Image.arrayjoin(frames, {across = 1})
+		:flatten{background = darkbg}
+	local final = joined:equal(darkbg):bandand()
+		:ifthenelse({0,0,0,0},alpha.add(joined))
+	
 	final:set_type(24ULL, "page-height", frames[1]:height())
 	
 	final:write_to_file(path.resolve(o))
